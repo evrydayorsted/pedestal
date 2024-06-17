1728211cdd3c35969be7ad836727e4eef1073b13
evrydayorsted <collinfitz2001>
2024-06-17 10:24:35 -0400

    testing how long download takes

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from Shot import *
-
+import time
+start_time = time.time()
 a = Shot(49239, "all")
 
 
 # Adding new shots
 a.fit(savepklforshot=True)
+print("--- %s seconds ---" % (time.time() - start_time))
+a.contourPlot(1)
