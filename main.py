229be5271718bde9df6b55ab5dba9edbe017a7e6
cfitzpat <collinfitz2001@gmail.com>
2024-06-18 12:41:32 -0400

    adding more shots

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ start_time = time.time()
 newShots = pandas.read_csv("MU03_shotlist_cleaned.csv")
 counter = 1
 for i in newShots["Shot Number"]:
-    if i>49391:
+    if i>49413:
         a = Shot(i, "all")
         # Adding new shots
         a.fit(savepklforshot=True)
         print("--- %s seconds ---" % (time.time() - start_time))
-        print(str(69-counter)+"left to go")
+        print(str(52-counter)+"left to go")
         counter += 1
 
