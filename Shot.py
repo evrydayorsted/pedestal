4c3443956ae0c6106fa32d89153e03319222ebef
evrydayorsted <collinfitz2001>
2024-06-18 14:14:14 -0400

    90

--- Shot.py
+++ Shot.py
@@ -662,8 +662,8 @@ class Shot:
                     index, = np.where((xquantity>=xx[i])   & 
                                         (xquantity< xx[i+1]) &
                                         (yquantity>=yy[j])   &
-                                        (yquantity< yy[j+1]))#&  for plasma filtering
-                                      #  (self.Ip.data[np.argmin(self.Ip.time.data-self.times)]<0.9*np.max(self.Ip.data)))
+                                        (yquantity< yy[j+1])& 
+                                        (self.Ip.data[np.argmin(self.Ip.time.data-self.times)]<0.9*np.max(self.Ip.data)))
                     if len(index) >= numMin:
                         if countType == "count":
                             if len(index) == 0:
